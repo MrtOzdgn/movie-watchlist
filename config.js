09949f573fdc639fa461bb1ede5728d9bb835ede
Mert Özdoğan <mertozdogan@192.168.1.8>
2026-07-23 18:54:06 +0300

    Add Firebase project config

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 // security is enforced by the Firestore rules (see the setup notes you were
 // given), not by hiding this config. Still, this file is fine to commit.
 window.APP_CONFIG = {
-  TMDB_API_KEY: "PASTE_YOUR_TMDB_API_KEY_HERE",
+  TMDB_API_KEY: "387ace388cabfcabe0a27ef46b811937",
 
   FIREBASE_CONFIG: {
-    apiKey: "PASTE_ME",
-    authDomain: "PASTE_ME",
-    projectId: "PASTE_ME",
-    storageBucket: "PASTE_ME",
-    messagingSenderId: "PASTE_ME",
-    appId: "PASTE_ME"
+    apiKey: "AIzaSyCqkUFatIQ3tQJoKCozKCZ9uOTzQHhqQFU",
+    authDomain: "movie-watchlist-3bf65.firebaseapp.com",
+    projectId: "movie-watchlist-3bf65",
+    storageBucket: "movie-watchlist-3bf65.firebasestorage.app",
+    messagingSenderId: "513208011670",
+    appId: "1:513208011670:web:0e9cb30a2168aeee8babb7"
   }
 };
